659f253a9ccbb0f79edba34875a396088e888435
jennermaxim <jennersi1remaxim@gmail.com>
2025-01-08 17:14:41 +0300

    Add the Online config file

--- localhost config/config.php
+++ localhost config/config.php
@@ -1,11 +1,15 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'automend_food');
-define('DB_PASSWORD', 'Food@Ney@Cuu');
-define('DB_NAME', 'automend_food');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'fds_db');
 
+/* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+// Check connection
 if ($conn === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
